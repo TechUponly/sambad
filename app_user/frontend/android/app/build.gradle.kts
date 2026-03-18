5f21acb0a61bd29675eb537fff37b187a9f3fc7d
SAURABH DOKHALE <saurabh.dokhale@uponlytech.com>
2026-03-18 19:56:25 +0530

    Update build.gradle.kts

--- app_user/frontend/android/app/build.gradle.kts
+++ app_user/frontend/android/app/build.gradle.kts
@@ -4,26 +4,20 @@ plugins {
     id("dev.flutter.flutter-gradle-plugin")
 }
 
-import java.util.Properties
-import java.io.FileInputStream
-
-val keystoreProperties = Properties()
-val keystorePropertiesFile = rootProject.file("key.properties")
-if (keystorePropertiesFile.exists()) {
-    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
-}
-
 android {
     namespace = "com.shamrai.sambad"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
+
     kotlinOptions {
         jvmTarget = JavaVersion.VERSION_17.toString()
     }
+
     signingConfigs {
         create("customDebug") {
             storeFile = file("${System.getProperty("user.home")}/.android/debug.keystore")
@@ -31,13 +25,8 @@ android {
             keyAlias = "androiddebugkey"
             keyPassword = "android"
         }
-        create("release") {
-            keyAlias = keystoreProperties["keyAlias"] as String?
-            keyPassword = keystoreProperties["keyPassword"] as String?
-            storeFile = keystoreProperties["storeFile"]?.let { file(it.toString()) }
-            storePassword = keystoreProperties["storePassword"] as String?
-        }
     }
+
     defaultConfig {
         applicationId = "com.shamrai.sambad"
         minSdk = flutter.minSdkVersion
@@ -45,15 +34,17 @@ android {
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
+
     buildTypes {
         release {
-            signingConfig = signingConfigs.getByName("release")
+            signingConfig = signingConfigs.getByName("customDebug")
         }
         debug {
             signingConfig = signingConfigs.getByName("customDebug")
         }
     }
 }
+
 flutter {
     source = "../.."
 }
